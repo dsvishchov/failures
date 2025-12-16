43362ada3f3aa0c6e9c3d4aa8c0234ebde6e576d
Dmytro Svishchov <dsvishchov@gmail.com>
2025-12-16 18:24:05 +0100

    Change annotation name to FailureError

--- packages/failures/lib/failures.dart
+++ packages/failures/lib/failures.dart
@@ -1,6 +1,6 @@
 library;
 
-export 'src/annotations/make_failure_annotation.dart';
+export 'src/annotations/failure_error_annotation.dart';
 export 'src/failures/dio_failure.dart';
 export 'src/failures/failure.dart';
 export 'src/failures/generic_failure.dart';
